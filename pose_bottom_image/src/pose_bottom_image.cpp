4de67671e9b07167e1925bb1713ae6e4e08db6e3
jinfu_leng <jinfu.leng@gmail.com>
2013-03-11 14:14:54 -0500

    cleaned the code

--- pose_bottom_image/src/pose_bottom_image.cpp
+++ pose_bottom_image/src/pose_bottom_image.cpp
@@ -63,17 +63,6 @@ void PoseBottomImage::callbackARPoseMarkerMsg(const ar_pose::ARMarker &ar_pose_m
 	//printf("new orientation:\n %lf\n %lf\n %lf\n",subjectPose.rotation.x,subjectPose.rotation.y,subjectPose.rotation.z);
 }
 
-// other functions
-/*void HoverCurrentPosition::FlytoPoint(double x, double y, double z, double w){
-	//collab_msgs::SubjectPose subject_pose = UAV_subject_pose_;
-	collab_msgs::SubjectPose subject_pose;
-	subject_pose.translation.x = x;
-	subject_pose.translation.y = y;
-	subject_pose.translation.z = z;
-	subject_pose.rotation.z = w;
-	UAV_subject_pose_pub_.publish(subject_pose);
-}*/
-
 // main
 int main(int argc, char** argv){
 	ros::init(argc, argv, "PoseBottomImage");
