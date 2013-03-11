29ac865d1076e0017bc9d40725d33c08d0a21569
jinfu_leng <jinfu.leng@gmail.com>
2013-03-11 10:43:37 -0500

    added some launch files

---
+++ launch/start_balldetector.sh
@@ -0,0 +1,5 @@
+#!\bin\sh
+
+echo "launch ballDetector"
+source /opt/ros/fuerte/setup.bash
+roslaunch ~/ros/projects/launch/ballDetector_bluefox.launch
---
+++ launch/start_rxtx_send.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "launch rxtx_send"
+source /opt/ros/fuerte/setup.bash
+roslaunch ~/ros/projects/launch/rxtx_send.launch
