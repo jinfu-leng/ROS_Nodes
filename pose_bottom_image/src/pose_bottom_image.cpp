d04f08450af218e13facc65df798d60bc4bd2746
jinfu_leng <jinfu.leng@gmail.com>
2013-03-26 14:02:22 -0500

    fixed the bug of empty header

--- pose_bottom_image/src/pose_bottom_image.cpp
+++ pose_bottom_image/src/pose_bottom_image.cpp
@@ -17,33 +17,37 @@ private:
 
 	// subscribe	
 	ros::Subscriber ar_pose_marker_sub_;
+	ros::Subscriber UAV_subject_pose_sub_; // from global
 
 	// publish
 	ros::Publisher camera_subject_pose_pub_;
 
 	// other parameters
-	//collab_msgs::SubjectPose camera_subject_pose_;	
+	collab_msgs::SubjectPose UAV_subject_pose_;
 
 	// callback functions
 	void callbackARPoseMarkerMsg(const ar_pose::ARMarker &ar_pose_marker_msg);
+	void callbackUAVSubjectPoseMsg(const collab_msgs::SubjectPose &subject_pose_msg); // get global subject pose
 };
 
 // construction
 PoseBottomImage::PoseBottomImage(){	
 	// subscribe
 	ar_pose_marker_sub_ = nh.subscribe("ar_pose_marker", 1, &PoseBottomImage::callbackARPoseMarkerMsg, this);
+	UAV_subject_pose_sub_ = nh.subscribe("global_subject_pose", 1, &PoseBottomImage::callbackUAVSubjectPoseMsg, this);
 	
 	// publish
   	camera_subject_pose_pub_ = nh.advertise<collab_msgs::SubjectPose>("camera_subject_pose", 1, true);
 }
 
 // callback functions
+void PoseBottomImage::callbackUAVSubjectPoseMsg(const collab_msgs::SubjectPose &subject_pose_msg){
+	UAV_subject_pose_ = subject_pose_msg;	
+}
+
 void PoseBottomImage::callbackARPoseMarkerMsg(const ar_pose::ARMarker &ar_pose_marker_msg){
 	geometry_msgs::Point  position= ar_pose_marker_msg.pose.pose.position;
 	geometry_msgs::Quaternion quaternion = ar_pose_marker_msg.pose.pose.orientation;
-	//printf("old position:\n %lf\n %lf\n %lf\n",position.x,position.y,position.z);
-	//printf("old orientation:\n %lf\n %lf\n %lf\n %lf\n",quaternion.x,quaternion.y,quaternion.z,quaternion.w);
-	
 	
 	tf::Quaternion q;
 	double roll, pitch, yaw;
@@ -56,8 +60,11 @@ void PoseBottomImage::callbackARPoseMarkerMsg(const ar_pose::ARMarker &ar_pose_m
 	subjectPose.translation.z = position.z;
 	//subjectPose.rotation.x = pitch;
 	//subjectPose.rotation.y = roll;
+	subjectPose.rotation.x = UAV_subject_pose_.rotation.x;
+	subjectPose.rotation.y = UAV_subject_pose_.rotation.y;
 	subjectPose.rotation.z = -yaw;
 	if(subjectPose.rotation.z<0) subjectPose.rotation.z = 2*PI + subjectPose.rotation.z;
+	subjcetPose.header.stamp = ros::Time::now();
 	camera_subject_pose_pub_.publish(subjectPose);
 	//printf("new position:\n %lf\n %lf\n %lf\n",subjectPose.translation.x,subjectPose.translation.y,subjectPose.translation.z);
 	//printf("new orientation:\n %lf\n %lf\n %lf\n",subjectPose.rotation.x,subjectPose.rotation.y,subjectPose.rotation.z);
