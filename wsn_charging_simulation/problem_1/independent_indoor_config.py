3c9c22a40fea70d714df4521a8e6815be0c6439e
jinfu_leng <jinfu.leng@gmail.com>
2013-09-09 13:46:16 -0500

    updated the parameters

--- wsn_charging_simulation/problem_1/independent_indoor_config.py
+++ wsn_charging_simulation/problem_1/independent_indoor_config.py
@@ -8,7 +8,7 @@ param_node_power_capacity = 60
 param_node_power_consumption_rate = 1
 param_node_initial_power = param_node_power_capacity
 
-param_UAV_power_capacity = 25 * 3600 * 1.0 
+param_UAV_power_capacity = 25.0 * 3600
 param_UAV_flight_power_consumption_rate = 75.0
 param_UAV_initial_power = param_UAV_power_capacity
 param_UAV_charging_power_consumption_rate = 45.0
