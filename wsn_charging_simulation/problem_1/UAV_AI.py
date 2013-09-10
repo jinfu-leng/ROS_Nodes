6d3ee0c2376f0668328e161a50a0602fa8545af0
jinfu_leng <jinfu.leng@gmail.com>
2013-09-10 10:39:13 -0500

    added threshold to next_second_least_power and fixed a bug in computing power consumption

--- wsn_charging_simulation/problem_1/UAV_AI.py
+++ wsn_charging_simulation/problem_1/UAV_AI.py
@@ -21,7 +21,21 @@ def is_UAV_able_back_home_after_next_second(UAV):
 	speed = UAV['speed']
 	return int(next_power/consume_rate) > (int(distance_home/speed) + 3)
 
-def next_second_least_power(UAV, nodes):
+def is_UAV_at_home(UAV):
+	if UAV['current_x'] == UAV['home_x'] and UAV['current_y'] == UAV['home_y']:
+		return True
+	else:
+		return False
+
+def next_node_least_power(nodes, threshold):
+	sorted_nodes = sorted(nodes, key=lambda node: node['power'])
+	node_least_power = sorted_nodes[0]
+	if node_least_power['power']/node_least_power['capacity'] <= threshold:
+		return node_least_power['id']
+	else:
+		return None
+
+def next_second_least_power(UAV, nodes, threshold = 1.0):
 	if UAV['status'] != 'back' and UAV['status'] != 'chargingself' and is_UAV_able_back_home_after_next_second(UAV) == False:
 		UAV['status'] = 'back'
 		UAV['task_num'] += 1
@@ -43,13 +57,16 @@ def next_second_least_power(UAV, nodes):
 			UAV['power'] += UAV['charged_power_rate']
 	
 	if UAV['status'] == 'idle':
-		sorted_nodes = sorted(nodes, key=lambda node: node['power'])
-		UAV['dest_node_id'] = sorted_nodes[0]['id']
-		UAV['status'] = 'flying'
+		if is_UAV_at_home(UAV) == True:
+			next_node_id = next_node_least_power(nodes, threshold)
+		else:
+			next_node_id = next_node_least_power(nodes, 1.0)
+		if next_node_id != None:
+			UAV['dest_node_id'] = next_node_id
+			UAV['status'] = 'flying'
 
 	if UAV['status'] == 'flying':
-		if nodes[UAV['dest_node_id']]['x'] == UAV['current_x'] and nodes[UAV['dest_node_id']]['y'] == UAV['current_y']:
-			UAV['power'] -= UAV['flght_power_rate']
+		if nodes[UAV['dest_node_id']]['x'] == UAV['current_x'] and nodes[UAV['dest_node_id']]['y'] == UAV['current_y']:			
 			UAV['status'] = 'charging'
 		else:
 			next_x, next_y = next_position(UAV['current_x'], UAV['current_y'], nodes[UAV['dest_node_id']]['x'], nodes[UAV['dest_node_id']]['y'], UAV['speed'])
@@ -59,6 +76,7 @@ def next_second_least_power(UAV, nodes):
 		
 	if UAV['status'] == 'charging':
 		UAV['power'] -= UAV['charging_power_rate']
+		UAV['power'] -= UAV['flght_power_rate']
 		nodes[UAV['dest_node_id']]['power'] += UAV['charging_power_rate'] * UAV['transfer_rate']
 		nodes[UAV['dest_node_id']]['power'] = min(nodes[UAV['dest_node_id']]['power'], nodes[UAV['dest_node_id']]['capacity'])
 		if nodes[UAV['dest_node_id']]['power'] == nodes[UAV['dest_node_id']]['capacity']:
@@ -66,7 +84,7 @@ def next_second_least_power(UAV, nodes):
 			UAV['dest_node_id'] = None
 
 def next_second(UAV, nodes, mode = 'least_power'):
-	if mode == 'least_power':
-		next_second_least_power(UAV, nodes)
+	if mode == 'least_power': # 1. charge the node with the least power 2. the threshold decides what time to go outside of base
+		next_second_least_power(UAV, nodes, 0.2) 
 	else:
 		print 'error'
