e96bcb8d1a8f567ce4659f0b1221fd06e0552c66
jinfu-leng <jinfu.leng@gmail.com>
2013-09-29 20:19:48 -0500

    being able to save result to .csv file

--- wsn_charging_simulation/problem_1/start_experiments.py
+++ wsn_charging_simulation/problem_1/start_experiments.py
@@ -1,10 +1,16 @@
-import os
+import os, copy
 from object_manager import ObjectManager
 import UAV_AI
 
 
-UAV_modes = ['least_power', 'least_power_k']
-time_limits = [604800]
+import outdoor_faraway_config as config
+param_number_nodes = [10, 20]
+param_ground_size = [100, 500]
+param_network_type = ['homogeneous']
+param_UAV_modes = ['least_power', 'least_power_partition']
+param_time_limits = [604800]
+param_res_file_name = 'res_outdoor_faraway.csv'
+
 
 def get_all_file(dir_path, file_extension):
 	file_list=os.listdir(dir_path)
@@ -30,23 +36,48 @@ def nodes_next_second(nodes):
 	for node in nodes:
 		node['power'] -= node['rate']
 
+
+# create object manager to create system
 object_manager_ = ObjectManager()
-object_files = get_all_file('.', '.csv')
-for object_file in object_files:
-	for UAV_mode in UAV_modes:
-		for time_limit in time_limits:
-			round_num = 1
-			UAV, nodes = object_manager_.read_objects(object_file)
-			print 'UAV mode: ' + UAV_mode
-			print 'System data: ' + object_file
-			while is_valid_node_network(nodes) and is_valid_UAV(UAV):
-				if round_num == time_limit:
-					print 'The system was valid during the past ' + str(time_limit) + ' seconds'
-					print 'The charging task was conducted ' + str(UAV['task_num']) + ' times'
-					break
-				round_num += 1
-				nodes_next_second(nodes)
-				UAV_AI.next_second(UAV, nodes, UAV_mode)
-			else:
-				print 'The system is not valid at round ' + str(round_num)
-			print
+
+# write the first line of the result
+res_file = open(param_res_file_name, 'w')
+res_file.write('UAV_method,number_nodes,ground_width,ground_height,network_type,time_limit,number_task\n')
+
+# create the file
+for num in param_number_nodes:
+	for size in param_ground_size:
+		for net_type in param_network_type:
+			config.param_number_nodes = num
+			config.param_ground_width = size
+			config.param_ground_height = size
+			config.param_network_type = net_type
+			UAV_new, nodes_new = object_manager_.create_objects(config)
+			
+			# start the experiment
+			for UAV_mode in param_UAV_modes:
+				for time_limit in param_time_limits:
+					UAV = copy.deepcopy(UAV_new)
+					nodes = copy.deepcopy(nodes_new)
+					output_line = str(UAV_mode) + ',' + str(num) + ',' + str(size) + ',' + str(size) + ',' \
+						+ str(net_type) + ',' + str(time_limit)
+					round_num = 1
+					
+					print 'System config: ' + output_line
+					while is_valid_node_network(nodes) and is_valid_UAV(UAV):
+						if round_num == time_limit:
+							print 'The system was valid during the past ' + str(time_limit) + ' seconds'
+							print 'The charging task was conducted ' + str(UAV['task_num']) + ' times'
+							output_line += ',' + str(UAV['task_num'])
+							break
+						round_num += 1
+						nodes_next_second(nodes)
+						UAV_AI.next_second(UAV, nodes, UAV_mode)
+					else:
+						print 'The system is not valid at round ' + str(round_num)
+						output_line += ',' + str(-1)
+					print
+					# write one record
+					res_file.write(output_line + '\n')
+
+res_file.close()
