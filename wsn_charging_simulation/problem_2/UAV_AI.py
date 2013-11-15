d3a38071da28c5d479a56ea7066f55c0a4c0873f
Jinfu Leng <jinfu.leng@gmail.com>
2013-11-15 14:16:04 -0600

    relaxed the condition for making UAV to fly back added function to compute upper and lower bound added the constraint that UAV has to be at home when its power is 0 	modified:   UAV_AI.py

--- wsn_charging_simulation/problem_2/UAV_AI.py
+++ wsn_charging_simulation/problem_2/UAV_AI.py
@@ -20,7 +20,7 @@ def is_UAV_able_back_home_after_next_second(UAV):
 	consume_rate = UAV['flght_power_rate']	
 	distance_home = euclidean_distance(UAV['current_x'], UAV['current_y'], UAV['home_x'], UAV['home_y'])
 	speed = UAV['speed']
-	return int(next_power/consume_rate) > (int(distance_home/speed) + 3)
+	return int(next_power/consume_rate) > (int(distance_home/speed) + 1)
 
 def is_UAV_at_home(UAV):
 	if UAV['current_x'] == UAV['home_x'] and UAV['current_y'] == UAV['home_y']:
@@ -44,16 +44,21 @@ def shortest_distance_node_path(start_x, start_y, node_id_list, nodes):
 			best_path = path
 	return list(best_path)
 
+def compute_total_cycle_distance(UAV, nodes, path):
+	nodes_cnt = len(path)
+	total_distance = 0.0
+	for index in range(nodes_cnt-1):
+		total_distance += euclidean_distance(nodes[path[index]]['x'], nodes[path[index]]['y'], nodes[path[index + 1]]['x'], nodes[path[index + 1]]['y'])
+	total_distance += euclidean_distance(UAV['current_x'], UAV['current_y'], nodes[path[0]]['x'], nodes[path[0]]['y'])
+	total_distance += euclidean_distance(UAV['current_x'], UAV['current_y'], nodes[path[nodes_cnt - 1]]['x'], nodes[path[nodes_cnt - 1]]['y'])
+	return total_distance
+
 def hamiltonian_node_path(UAV, nodes):
 	nodes_cnt = len(nodes)
 	shortest_path = []
 	shortest_dist = 1000000000.0
 	for path in itertools.permutations(range(nodes_cnt)):
-		current_dist = 0.0
-		for index in range(nodes_cnt-1):
-			current_dist += euclidean_distance(nodes[path[index]]['x'], nodes[path[index]]['y'], nodes[path[index + 1]]['x'], nodes[path[index + 1]]['y'])
-		current_dist += euclidean_distance(UAV['current_x'], UAV['current_y'], nodes[path[0]]['x'], nodes[path[0]]['y'])
-		current_dist += euclidean_distance(UAV['current_x'], UAV['current_y'], nodes[path[nodes_cnt - 1]]['x'], nodes[path[nodes_cnt - 1]]['y'])
+		current_dist = compute_total_cycle_distance(UAV, nodes, path)
 		if current_dist < shortest_dist:
 			shortest_dist = current_dist
 			shortest_path = path
@@ -154,6 +159,14 @@ def next_second_closest_to_average(UAV, nodes):
 def next_second_dest_list(UAV, nodes, mode):
 	UAV['power'] -= UAV['flght_power_rate']
 
+	if UAV['status'] != 'back' and is_UAV_able_back_home_after_next_second(UAV) == False:
+		UAV['status'] = 'back'
+		
+	if UAV['status'] == 'back':
+		next_x, next_y = next_position(UAV['current_x'], UAV['current_y'], UAV['home_x'], UAV['home_y'], UAV['speed'])
+		UAV['current_x'] = next_x
+		UAV['current_y'] = next_y
+
 	if UAV['status'] == 'idle':
 		UAV['dest_list'] = hamiltonian_node_path(UAV, nodes)
 		UAV['status'] = 'looking'
@@ -214,3 +227,54 @@ def next_second(UAV, nodes, mode = 'cloeset_to_half'):
 		next_second_dest_list(UAV, nodes, mode)
 	else:
 		print 'error'
+
+	
+
+def find_best_lowest(values, more_value):
+	values.sort()
+	left = values[0]
+	right = values[0]+left
+	while right - left > 0.001:
+		mid = (left + right) / 2
+		current = more_value
+		for value in values:
+			if mid > value:
+				current -= mid-value
+		if current > 0:
+			left = mid
+		else:
+			right = mid
+	return left
+
+
+def compute_lifetime_upper_bound(UAV, nodes): # in practice, the node with lowerst power may die before charging it
+	sorted_nodes = sorted(nodes, key = lambda node: node['power'])
+	nodes_cnt = len(sorted_nodes)
+	max_lowest_node_power = nodes[0]['power']
+	for charging_node_cnt in range(1, nodes_cnt+1):
+		charging_node_list = sorted_nodes[:charging_node_cnt]
+		hamiltonian_path = hamiltonian_node_path(UAV, charging_node_list)
+		total_distance = compute_total_cycle_distance(UAV, charging_node_list, hamiltonian_path)
+		#print "hamiltonian_path", hamiltonian_path
+
+		total_power = UAV['power']
+		
+		flight_power = (total_distance / UAV['speed']) * UAV['flght_power_rate']
+		total_charging_power = total_power - flight_power
+
+		if total_charging_power <= 0:
+			break
+
+		total_charging_time = total_charging_power / (UAV['flght_power_rate'] + UAV['charging_power_rate'])
+		total_efficient_transfer_power =  total_charging_time * UAV['charging_power_rate'] * UAV['transfer_rate']
+
+		lowest_node_power = find_best_lowest([node['power'] for node in charging_node_list], total_efficient_transfer_power)
+		#print "lowest_node_power", lowest_node_power
+		max_lowest_node_power = max(max_lowest_node_power, lowest_node_power)
+
+	return int(max_lowest_node_power / nodes[0]['rate'])
+
+def compute_lifetime_lower_bound(UAV, nodes): # in practice, the node with lowerst power may die before charging it
+	sorted_nodes = sorted(nodes, key = lambda node: node['power'])	
+	return int(math.ceil(sorted_nodes[0]['power'] / nodes[0]['rate']))
+		
