851a89f55d4ffd862b7bb8ebec6836d67553b797
Jinfu Leng <jinfu.leng@gmail.com>
2013-11-20 16:13:35 -0600

    updated the output format

--- wsn_charging_simulation/problem_2/start_experiments.py
+++ wsn_charging_simulation/problem_2/start_experiments.py
@@ -8,8 +8,8 @@ param_number_nodes = [8]
 param_ground_size = [100]
 param_network_type = ['homogeneous2']
 param_UAV_modes = ['closest_to_half', 'hamiltonian_to_half', 'closest_to_average', 'hamiltonian_to_average', 'closest_below_average_to_full', 'hamiltonian_below_average_to_full', 'closest_to_full', 'hamiltonian_to_full']
-param_experiment_time = 1
-param_res_file_name = 'res_prob2_outdoor_faraway_11_15.csv'
+param_experiment_time = 5
+param_res_file_name = 'res_prob2_outdoor_faraway_11_20.csv'
 
 def is_valid_node_network(nodes):
 	for node in nodes:
@@ -32,7 +32,7 @@ object_manager_ = ObjectManager()
 
 # write the first line of the result
 res_file = open(param_res_file_name, 'w')
-res_file.write('UAV_method,number_nodes,ground_width,ground_height,network_type,work_time\n')
+res_file.write('network_type,UAV_mode,work_time\n')
 
 # create the file
 for num in param_number_nodes:
@@ -42,35 +42,34 @@ for num in param_number_nodes:
 			config.param_ground_width = size
 			config.param_ground_height = size
 			config.param_network_type = net_type
+			networ_type_str = str(num) + '_' + str(size) + '_' + str(size) + '_' + str(net_type)
 			for experiment_time in range(param_experiment_time):
 				UAV_new, nodes_new = object_manager_.create_objects(config)
 				# compute lifetime lower bound
 				UAV = copy.deepcopy(UAV_new)
 				nodes = copy.deepcopy(nodes_new)
-				print "lifttime lower bound:", UAV_AI.compute_lifetime_lower_bound(UAV, nodes)
+				lower_bound = UAV_AI.compute_lifetime_lower_bound(UAV, nodes)
+				print "Lifttime lower bound:", lower_bound
+				res_file.write(networ_type_str + ',' + 'lower_bound' + ',' + str(lower_bound) + '\n')
 				# compute lifetime upper bound
 				UAV = copy.deepcopy(UAV_new)
 				nodes = copy.deepcopy(nodes_new)
-				print "lifttime upper bound:", UAV_AI.compute_lifetime_upper_bound(UAV, nodes)		
+				upper_bound = UAV_AI.compute_lifetime_upper_bound(UAV, nodes)
+				print "Lifttime upper bound:", upper_bound
+				res_file.write(networ_type_str + ',' + 'upper_bound' + ',' + str(upper_bound) + '\n')
 				# start the experiment
 				for UAV_mode in param_UAV_modes:
 					UAV = copy.deepcopy(UAV_new)
 					nodes = copy.deepcopy(nodes_new)
-					output_line = str(UAV_mode) + ',' \
-						+ str(num) + ',' + str(size) + ',' + str(size) + ',' \
-						+ str(net_type)
 					round_num = 0					
-					print 'System config: ' + output_line
-					while is_valid_node_network(nodes):
-						
+					print 'Networ Type: ' + networ_type_str
+					while is_valid_node_network(nodes):						
 						nodes_next_second(nodes)
 						if is_valid_UAV(UAV):
 							UAV_AI.next_second(UAV, nodes, UAV_mode)
 						round_num += 1
-					output_line += ',' + str(round_num)
-					print 'The system is died at round ' + str(round_num)
+					print 'The system is dead at round ' + str(round_num)
 					print
-					# write one record
-					res_file.write(output_line + '\n')
+					res_file.write(networ_type_str + ',' + UAV_mode + ',' + str(round_num) + '\n')
 
 res_file.close()
