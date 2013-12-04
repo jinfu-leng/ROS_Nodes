b01bd30826b1307b856ad6f529a19f85cf7536da
Jinfu Leng <jinfu.leng@gmail.com>
2013-12-04 15:49:01 -0600

    updated test config to use the data in the paper

--- wsn_charging_simulation/problem_2/test_config.py
+++ wsn_charging_simulation/problem_2/test_config.py
@@ -9,12 +9,12 @@ param_node_power_consumption_rate = 0.1855
 param_node_initial_power = param_node_power_capacity
 
 param_UAV_power_capacity = 25.0 * 3600
-param_UAV_flight_power_consumption_rate = 120
-param_UAV_hovering_power_consumption_rate = 60
+param_UAV_flight_power_consumption_rate = 121.91
+param_UAV_hovering_power_consumption_rate = 92.28
 param_UAV_initial_power = param_UAV_power_capacity
-param_UAV_charging_power_consumption_rate = 20
+param_UAV_charging_power_consumption_rate = 20.0
 param_UAV_charging_power_transfer_rate = 0.2
-param_UAV_localization_time = 6.0
-param_UAV_moving_speed = 5.0
+param_UAV_localization_time = 10.0
+param_UAV_moving_speed = 7.33
 param_UAV_initial_x = param_ground_width / 2
 param_UAV_initial_y = param_ground_height / 2
