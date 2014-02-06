a71c51f7d1a8e04c2e2d85a067e026df40ab89b5
Jinfu Leng <jinfu.leng@gmail.com>
2014-02-06 10:13:45 -0600

    added a function to generate normalized figure for generate_figure_one_sensor_num

--- wsn_charging_simulation/problem_1/generate_figure_one_sensor_num.py
+++ wsn_charging_simulation/problem_1/generate_figure_one_sensor_num.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-input_file_name = 'data_one_center_sensor_num.csv'
+input_file_name = 'data_one_center_sensor_num_2.csv'
 
 def UAV_mode_label_matcher(UAV_mode):
 	matcher = {}
@@ -54,6 +54,45 @@ def draw_bar_err_group_figure(nodes):
 	plt.title('Lifetime by Sensor Node Numbers and Algorithms')
 	plt.xticks(left_coordinates + 0.5, group_list)
 
+def draw_normalized_bar_err_group_figure(nodes):
+	label_list = ['lower_bound', 'closest_to_full', 'closest_random',
+		'closest_with_constant', 'closest_to_initial_average', 'least_power_to_optimized_one_flight']
+
+	label_cnt = len(label_list)
+	bar_width = 1.0 / (label_cnt + 1)
+
+	group_set = set([node['group'] for node in nodes])
+	group_list = list(group_set)
+	group_list = sorted(group_list, key=lambda node:int(node))
+	group_cnt = len(group_list)
+	left_coordinates = np.arange(group_cnt)
+
+	colors = 'bgrcmyk'
+	ax = plt.subplot(111)
+	base_nodes = [node for node in nodes if node['label'] == label_list[0]]
+	base_nodes = sorted(base_nodes, key=lambda node:node['group'])
+	for i in range(len(label_list)):
+		label = label_list[i]
+		candidate_nodes = [node for node in nodes if node['label'] == label]
+		candidate_nodes = sorted(candidate_nodes, key=lambda node:node['group'])
+		value = [node['value'] / base['value'] for node, base in zip(candidate_nodes, base_nodes)]
+		error = [node['error'] / base['error'] for node, base in zip(candidate_nodes, base_nodes)]
+		ax.bar(left_coordinates + i * bar_width, value, bar_width,
+		label=UAV_mode_label_matcher(label), color=colors[i])
+	
+	# Shink current axis by 20%
+	box = ax.get_position()
+	ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+
+	# Put a legend to the right of the current axis
+	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+	plt.xlabel('Sensor Node Number')
+	plt.ylabel('Normalized Lifetime')
+	plt.title('Lifetime by Sensor Node Numbers and Algorithms')
+	plt.xticks(left_coordinates + 0.5, group_list)
+
+
 input_file = open(input_file_name, 'r')
 first_line = input_file.readline()
 
@@ -91,7 +130,7 @@ for network_type in network_types:
 		node['group'] = group
 		nodes.append(node)
 
-draw_bar_err_group_figure(nodes)
+draw_normalized_bar_err_group_figure(nodes)
 plt.show()
 
 
