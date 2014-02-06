38351c43be79d93aeaace55796d962b085f6c6c3
Jinfu Leng <jinfu.leng@gmail.com>
2014-02-06 15:10:25 -0600

    moved the common fuction for figure generating to a seperate file

---
+++ wsn_charging_simulation/problem_1/generate_figure_common.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def UAV_mode_label_matcher(UAV_mode):
+	matcher = {}
+	matcher['lower_bound'] = 'NO'
+	matcher['closest_to_full'] = 'FULL'
+	matcher['hamiltonian_to_full'] = 'FULL*'
+	matcher['closest_with_constant'] = 'FIX'
+	matcher['hamiltonian_with_constant'] = 'FIX*'
+	matcher['closest_to_initial_average'] = 'AVG'
+	matcher['hamiltonian_to_initial_average'] = 'AVG*'
+	matcher['closest_random'] = 'RND'
+	matcher['hamiltonian_random'] = 'RND*'
+	matcher['least_power_to_optimized_one_flight'] = 'LEAST'
+	return matcher[UAV_mode]
+
+
+def draw_bar_err_group_figure(nodes, xlabel, ylabel, title):
+	label_list = ['lower_bound', 'closest_to_full', 'closest_random',
+		'closest_with_constant', 'closest_to_initial_average', 'least_power_to_optimized_one_flight']
+
+	label_cnt = len(label_list)
+	bar_width = 1.0 / (label_cnt + 1)
+
+	group_set = set([node['group'] for node in nodes])
+	group_list = list(group_set)
+	group_list = sorted(group_list, key=lambda node:float(node))
+	group_cnt = len(group_list)
+	left_coordinates = np.arange(group_cnt)
+
+	colors = 'bgrcmyk'
+	ax = plt.subplot(111)
+	for i in range(len(label_list)):
+		label = label_list[i]
+		candidate_nodes = [node for node in nodes if node['label'] == label]
+		candidate_nodes = sorted(candidate_nodes, key=lambda node:node['group'])
+		value = [node['value'] / (24 * 3600) for node in candidate_nodes]
+		error = [node['error'] / (24 * 3600) for node in candidate_nodes]
+		ax.bar(left_coordinates + i * bar_width, value, bar_width,
+		label=UAV_mode_label_matcher(label), color=colors[i])
+	
+	# Shink current axis by 20%
+	box = ax.get_position()
+	ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+
+	# Put a legend to the right of the current axis
+	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.title(title)
+	plt.xticks(left_coordinates + 0.5, group_list)
+
+def draw_normalized_bar_err_group_figure(nodes, xlabel, ylabel, title):
+	label_list = ['lower_bound', 'closest_to_full', 'closest_random',
+		'closest_with_constant', 'closest_to_initial_average', 'least_power_to_optimized_one_flight']
+
+	label_cnt = len(label_list)
+	bar_width = 1.0 / (label_cnt + 1)
+
+	group_set = set([node['group'] for node in nodes])
+	group_list = list(group_set)
+	group_list = sorted(group_list, key=lambda node:int(node))
+	group_cnt = len(group_list)
+	left_coordinates = np.arange(group_cnt)
+
+	colors = 'bgrcmyk'
+	ax = plt.subplot(111)
+	base_nodes = [node for node in nodes if node['label'] == label_list[0]]
+	base_nodes = sorted(base_nodes, key=lambda node:node['group'])
+	for i in range(len(label_list)):
+		label = label_list[i]
+		candidate_nodes = [node for node in nodes if node['label'] == label]
+		candidate_nodes = sorted(candidate_nodes, key=lambda node:node['group'])
+		value = [node['value'] / base['value'] for node, base in zip(candidate_nodes, base_nodes)]
+		error = [node['error'] / base['error'] for node, base in zip(candidate_nodes, base_nodes)]
+		ax.bar(left_coordinates + i * bar_width, value, bar_width,
+		label=UAV_mode_label_matcher(label), color=colors[i])
+	
+	# Shink current axis by 20%
+	box = ax.get_position()
+	ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+
+	# Put a legend to the right of the current axis
+	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.title(title)
+	plt.xticks(left_coordinates + 0.5, group_list)
