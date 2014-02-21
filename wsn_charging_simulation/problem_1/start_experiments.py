c1385641c8f5ed99399e8ddde26daa04b6eb4e1a
Jinfu Leng <jinfu.leng@gmail.com>
2014-02-21 15:54:39 -0600

    added UAV_power_capacity based experiments

--- wsn_charging_simulation/problem_1/start_experiments.py
+++ wsn_charging_simulation/problem_1/start_experiments.py
@@ -10,16 +10,17 @@ param_UAV_localization_time = [36]
 param_transfer_rate = [0.2]
 param_UAV_hovering_power_consumption_rate = [92.28]
 param_UAV_charging_power_consumption_rate = [20]
-param_UAV_base_distance = [0, 500, 1000, 2000]
+param_UAV_base_distance = [0]
+param_UAV_power_capacity = [25.0 * 3600, 25.0 * 3600 * 2, 25.0 * 3600 * 4, 25.0 * 3600 * 8]
 param_time_limit = 604800 * 10000
 param_network_type = ['homogeneous2']
 param_charge_mode = ['random', 'to_full', 'to_initial_average', 'with_constant', 'to_optimized_one_flight']
 #param_charge_mode = ['with_constant']
 #param_path_mode = ['least_power', 'closest', 'hamiltonian']
-param_path_mode = ['least_power', 'closest', 'hamiltonian']
+param_path_mode = ['least_power', 'closest']
 param_task_threshold = [0.8]
 param_experiment_time = 100
-param_res_file_name = 'center_base_distance.csv'
+param_res_file_name = 'center_UAV_power_capacity.csv'
 
 is_single_flight = True
 
@@ -75,77 +76,80 @@ for experiment_time in range(param_experiment_time):
 							for charging_rate in param_UAV_charging_power_consumption_rate: #charging energy consumption rate
 								for task_threshold in param_task_threshold:
 									for base_distance in param_UAV_base_distance:
-										config.param_number_nodes = num
-										config.param_ground_width = size
-										config.param_ground_height = size
-										config.param_UAV_localization_time = localization_time
-										config.param_UAV_charging_power_transfer_rate = transfer_rate
-										config.param_network_type = net_type
-										config.param_UAV_hovering_power_consumption_rate = hovering_rate
-										config.param_UAV_charging_power_consumption_rate = charging_rate
-										config.param_UAV_base_distance = base_distance
-										network_type_str = str(num) + '_' + str(size) + '_' + str(size) \
-											+ '_' + str(net_type) + '_' + str(localization_time) \
-											+ '_' + str(transfer_rate) + '_' + str(task_threshold) \
-											+ '_' + str(hovering_rate) + '_' + str(charging_rate) \
-											+ '_' + str(base_distance)
-										print 'Network Type: ' + network_type_str							
-										UAV_new, nodes_new = object_manager_.create_objects(config)
+										for UAV_power_capacity in param_UAV_power_capacity:
+											config.param_number_nodes = num
+											config.param_ground_width = size
+											config.param_ground_height = size
+											config.param_UAV_localization_time = localization_time
+											config.param_UAV_charging_power_transfer_rate = transfer_rate
+											config.param_network_type = net_type
+											config.param_UAV_hovering_power_consumption_rate = hovering_rate
+											config.param_UAV_charging_power_consumption_rate = charging_rate
+											config.param_UAV_base_distance = base_distance
+											config.param_UAV_power_capacity = UAV_power_capacity
+											config.param_UAV_initial_power = UAV_power_capacity
+											network_type_str = str(num) + '_' + str(size) + '_' + str(size) \
+												+ '_' + str(net_type) + '_' + str(localization_time) \
+												+ '_' + str(transfer_rate) + '_' + str(task_threshold) \
+												+ '_' + str(hovering_rate) + '_' + str(charging_rate) \
+												+ '_' + str(base_distance) + '_' + str(UAV_power_capacity)
+											print 'Network Type: ' + network_type_str							
+											UAV_new, nodes_new = object_manager_.create_objects(config)
 
-										# single flight
-										if is_single_flight == True:
-											# lower bound
-											nodes = copy.deepcopy(nodes_new)
-											lower_bound = UAV_AI.compute_lifetime_lower_bound(nodes)
-											print 'Lifetime lower Bound', lower_bound
-											res_file.write(network_type_str + ',' + 'lower_bound' + ',' + str(lower_bound) + '\n')	
+											# single flight
+											if is_single_flight == True:
+												# lower bound
+												nodes = copy.deepcopy(nodes_new)
+												lower_bound = UAV_AI.compute_lifetime_lower_bound(nodes)
+												print 'Lifetime lower Bound', lower_bound
+												res_file.write(network_type_str + ',' + 'lower_bound' + ',' + str(lower_bound) + '\n')	
 
-											# start the experiment
-											for charge_mode in param_charge_mode:
-												for path_mode in param_path_mode:
-													# filter invalid combination
-													if is_valid_combination(charge_mode, path_mode) == False:
-														continue
-													UAV_mode = path_mode + '_' + charge_mode
-													print 'UAV Mode: ' + UAV_mode
-													UAV = copy.deepcopy(UAV_new)
-													nodes = copy.deepcopy(nodes_new)
-													params = {}
-													round_num = 0
-													while is_valid_node_network(nodes) and is_valid_UAV(UAV) and round_num < param_time_limit:						
-														nodes_next_second(nodes)
-														UAV_AI.next_second(UAV, nodes, charge_mode, path_mode, task_threshold, params)
-														round_num += 1
-													round_num += int(min(node['power'] for node in nodes) / nodes[0]['rate'])
-													print 'The system is dead at round: ' + str(round_num)
-													res_file.write(network_type_str + ',' + UAV_mode + ',' + str(round_num) + '\n')
-										# multi flight
-										else:
-											for charge_mode in param_charge_mode:
-												for path_mode in param_path_mode:
-													# filter invalid combination
-													if is_valid_combination(charge_mode, path_mode) == False:
-														continue
-													UAV_mode = path_mode + '_' + charge_mode
-													print 'UAV Mode: ' + UAV_mode
-													UAV = copy.deepcopy(UAV_new)
-													nodes = copy.deepcopy(nodes_new)
-													params = {}
-													round_num = 0
-													UAV['flight_number'] = 0						
-													while is_valid_node_network(nodes) and is_valid_UAV(UAV) and round_num < param_time_limit:						
-														nodes_next_second(nodes)
-														UAV_AI.next_second(UAV, nodes, charge_mode, path_mode, task_threshold, params)
-														round_num += 1
-													
-													if round_num == param_time_limit:
-														print 'Number of flight: ' + str(UAV['flight_number'])
-														res_file.write(network_type_str + ',' + UAV_mode + ',' + str(UAV['flight_number']) + '\n')
-													else:
+												# start the experiment
+												for charge_mode in param_charge_mode:
+													for path_mode in param_path_mode:
+														# filter invalid combination
+														if is_valid_combination(charge_mode, path_mode) == False:
+															continue
+														UAV_mode = path_mode + '_' + charge_mode
+														print 'UAV Mode: ' + UAV_mode
+														UAV = copy.deepcopy(UAV_new)
+														nodes = copy.deepcopy(nodes_new)
+														params = {}
+														round_num = 0
+														while is_valid_node_network(nodes) and is_valid_UAV(UAV) and round_num < param_time_limit:
+															nodes_next_second(nodes)
+															UAV_AI.next_second(UAV, nodes, charge_mode, path_mode, task_threshold, params)
+															round_num += 1
+														round_num += int(min(node['power'] for node in nodes) / nodes[0]['rate'])
 														print 'The system is dead at round: ' + str(round_num)
-														res_file.write(network_type_str + ',' + UAV_mode + ',' + str(-1 * round_num) + '\n')	
-														if is_valid_UAV(UAV) == False:
-															print '!!!!!!!!!!!!!!!!It is beacause of the UAV!!!!!!!!!!!!!!!!!!'
+														res_file.write(network_type_str + ',' + UAV_mode + ',' + str(round_num) + '\n')
+											# multi flight
+											else:
+												for charge_mode in param_charge_mode:
+													for path_mode in param_path_mode:
+														# filter invalid combination
+														if is_valid_combination(charge_mode, path_mode) == False:
+															continue
+														UAV_mode = path_mode + '_' + charge_mode
+														print 'UAV Mode: ' + UAV_mode
+														UAV = copy.deepcopy(UAV_new)
+														nodes = copy.deepcopy(nodes_new)
+														params = {}
+														round_num = 0
+														UAV['flight_number'] = 0						
+														while is_valid_node_network(nodes) and is_valid_UAV(UAV) and round_num < param_time_limit:						
+															nodes_next_second(nodes)
+															UAV_AI.next_second(UAV, nodes, charge_mode, path_mode, task_threshold, params)
+															round_num += 1
 														
-										print
+														if round_num == param_time_limit:
+															print 'Number of flight: ' + str(UAV['flight_number'])
+															res_file.write(network_type_str + ',' + UAV_mode + ',' + str(UAV['flight_number']) + '\n')
+														else:
+															print 'The system is dead at round: ' + str(round_num)
+															res_file.write(network_type_str + ',' + UAV_mode + ',' + str(-1 * round_num) + '\n')	
+															if is_valid_UAV(UAV) == False:
+																print '!!!!!!!!!!!!!!!!It is beacause of the UAV!!!!!!!!!!!!!!!!!!'
+															
+											print
 res_file.close()
