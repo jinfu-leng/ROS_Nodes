86e3fad9b85bf0ff892826d5a0b4c6acea4ddc3d
Jinfu Leng <jinfu.leng@gmail.com>
2014-03-06 14:36:59 -0600

    added distance as a variable to the test_config

--- wsn_charging_simulation/problem_1/test_config.py
+++ wsn_charging_simulation/problem_1/test_config.py
@@ -1,14 +1,14 @@
 # configure file for test environment
-param_number_nodes = 20
-param_ground_width = 2000.0
-param_ground_height = 2000.0
+param_number_nodes = 5
+param_ground_width = 200.0
+param_ground_height = 200.0
 param_network_type = 'homogeneous2'
 
 param_node_power_capacity = 2.34 * 3600
 param_node_power_consumption_rate = param_node_power_capacity / (60 * 24 * 3600)
 param_node_initial_power = param_node_power_capacity
 
-param_UAV_power_capacity = 25.0 * 3600
+param_UAV_power_capacity = 25.0 * 3600 * 10
 param_UAV_flight_power_consumption_rate = 121.91
 param_UAV_hovering_power_consumption_rate = 92.28
 
@@ -17,7 +17,8 @@ param_UAV_charging_power_consumption_rate = 20.0
 param_UAV_charging_power_transfer_rate = 0.2
 param_UAV_localization_time = 10.0
 param_UAV_moving_speed = 7.33
-param_UAV_initial_x = param_ground_width / 2
+param_UAV_base_distance = 0
+param_UAV_initial_x = param_ground_width / 2 + param_UAV_base_distance
 param_UAV_initial_y = param_ground_height / 2
 
 # base station recharging only
