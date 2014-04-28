d1ce3ee7791e9058f8f47577d7d1a7f6a81d1358
Jinfu Leng <jinfu.leng@gmail.com>
2014-04-28 11:31:51 -0500

    cleaned test_config.py

--- wsn_charging_simulation/problem_1/test_config.py
+++ wsn_charging_simulation/problem_1/test_config.py
@@ -1,25 +1,25 @@
 # configure file for test environment
-param_number_nodes = 8
-param_ground_width = 200.0
-param_ground_height = 200.0
-param_network_type = 'homogeneous2'
+# Variables with None value are initialized in either start_experiments.py or object_manager.py
+param_number_nodes = None
+param_ground_width = None
+param_ground_height = None
+param_network_type = None
 
 param_node_power_capacity = 2.34 * 3600
 param_node_power_consumption_rate = param_node_power_capacity / (60 * 24 * 3600)
 param_node_initial_power = param_node_power_capacity
 
-param_UAV_power_capacity = 25.0 * 3600
+param_UAV_power_capacity = None
 param_UAV_flight_power_consumption_rate = 121.91
-param_UAV_hovering_power_consumption_rate = 92.28
+param_UAV_hovering_power_consumption_rate = None
 
-param_UAV_initial_power = param_UAV_power_capacity
-param_UAV_charging_power_consumption_rate = 20.0
-param_UAV_charging_power_transfer_rate = 0.2
-param_UAV_localization_time = 10.0
+param_UAV_charging_power_consumption_rate = None
+param_UAV_charging_power_transfer_rate = None
+param_UAV_localization_time = None
 param_UAV_moving_speed = 7.33
-param_UAV_base_distance = 0
-param_UAV_initial_x = param_ground_width / 2 + param_UAV_base_distance
-param_UAV_initial_y = param_ground_height / 2
+param_UAV_base_distance = None
+param_UAV_initial_x = None
+param_UAV_initial_y = None
 
-# base station recharging only
+# Base station recharging only (not implemented yet)
 param_UAV_accumulating_power_rate = 0
